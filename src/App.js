df3d33fa55096c28ef9b9120ecfe8c52f32850e7
Mohitb07 <bmohit980@gmail.com>
2023-01-09 16:24:22 +0530

    fix: connection to backend

--- src/App.js
+++ src/App.js
@@ -1,74 +1,78 @@
+import { useState } from "react";
 
-import './App.css';
-import Card from '../src/components/card';
+import { Box, Typography } from "@mui/material";
+import axios from "axios";
+
+import Card from "../src/components/card";
 import Board from "./components/board";
-import { Grid, Box, Typography } from '@mui/material';
-import UserprojectmapService from './Services/UserprojectmapService';
-import { useState } from 'react';
-
-
-const userprojectmap =(e) => {
-  e.preventDefault();
-  let empId = sessionStorage.getItem("empId");
-  let groupCode = sessionStorage.getItem("groupCode");
-  const userprojectmap={
-    empId,groupCode, 
-  };
-  UserprojectmapService.adduser(userprojectmap);
-
-}
-
+import { MASTER_API_URL } from "./api";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const handleUserProjectMap = async () => {
+    let empId = sessionStorage.getItem("empId");
+    let groupCode = sessionStorage.getItem("groupCode");
+    const userprojectmap = {
+      empId,
+      groupCode,
+    };
+    const response = await axios.post(
+      `${MASTER_API_URL}/upm/save`,
+      userprojectmap
+    );
+    console.log("do something with response", response.data);
+    setData(response.data);
+  };
+
+  console.log("data", data);
+
   return (
     <div className="App">
       <h1>Timesheet</h1>
-           
-      <main className='flexbox'>
+
+      <main className="flexbox">
         <Box>
-          <Typography className="name" >
+          <Typography className="name">
             <p>Employee Id : </p>
           </Typography>
-          <Typography className="name1" >
+          <Typography className="name1">
             <p>Group Code: </p>
           </Typography>
-      </Box>
-        
-   
-<Board id="form-1" className="form">
-  {/* this coming from backend */}
-  <h2>Available Projects</h2>
-  <Card id="card-1" className="card" draggable="true">
-    <p>Project 1</p>
-    
-  </Card>
-  <Card id="card-2" className="card" draggable="true">
-    <p>Project 2</p>
-  </Card>
-  <Card id="card-3" className="card" draggable="true">
-    <p>Project 3</p>
-  </Card>
-  <Card id="card-3" className="card" draggable="true">
-    <p>Project 4</p>
-  </Card>
-  <Card id="card-4" className="card" draggable="true">
-    <p>Demo</p>
-  </Card>
-  <Card id="card-5" className="card" draggable="true">
-    <p>Demo</p>
-  </Card>
-</Board>
-<Board id="form-2" className="form">
+        </Box>
 
-  <h2>Selected Projects</h2>
-  {/* <Card id="card-2" className="card" draggable="true">
+        <Board id="form-1" className="form">
+          {/* this coming from backend */}
+          <h2>Available Projects</h2>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Project 1</p>
+          </Card>
+          <Card id="card-2" className="card" draggable="true">
+            <p>Project 2</p>
+          </Card>
+          <Card id="card-3" className="card" draggable="true">
+            <p>Project 3</p>
+          </Card>
+          <Card id="card-3" className="card" draggable="true">
+            <p>Project 4</p>
+          </Card>
+          <Card id="card-4" className="card" draggable="true">
+            <p>Demo</p>
+          </Card>
+          <Card id="card-5" className="card" draggable="true">
+            <p>Demo</p>
+          </Card>
+        </Board>
+        <Board id="form-2" className="form">
+          <h2>Selected Projects</h2>
+          {/* <Card id="card-2" className="card" draggable="true">
     <p>CAFCS</p>
   </Card> */}
-</Board>
-
+        </Board>
       </main>
-      <div className='button'>
-        <button onClick={userprojectmap}>Save</button>
+      <div className="button">
+        <button onClick={handleUserProjectMap}>Save</button>
       </div>
     </div>
   );
--- src/Services/UserprojectmapService.js
+++ src/Services/UserprojectmapService.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-const MASTER_API_URL = "http://localhost:8001"
+// import axios from "axios";
 
-class userprojectmap{
-    adduser = (user)=> {
-        return axios.post(`${MASTER_API_URL}/upm/save`,user)
-    }
-}
-export default new userprojectmap;
+
+// class userprojectmap{
+//     adduser = (user)=> {
+//         return axios.post(`${MASTER_API_URL}/upm/save`,user)
+//     }
+// }
+// export default new userprojectmap;
